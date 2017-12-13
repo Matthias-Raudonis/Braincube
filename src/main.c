76418d01bebed0884cd74d4de9f9a4c0cbee422f
matthias <m.raudonis@web.de>
2017-12-13 23:07:22 +0100

    implementing command line compiling

--- src/main.c
+++ src/main.c
@@ -35,29 +35,41 @@ uint8_t cube[7][9];
 uint8_t pointer;
 uint32_t c; // instruction counter
 uint32_t c_p; // instruction pointer
-uint8_t program[SIZE_PROG];
-uint8_t stack[SIZE_STACK];
-uint8_t stackp;
+uint32_t program[SIZE_PROG][2];
+uint32_t stack[SIZE_STACK];
+uint32_t stackp;
 
-int main()
+int main(int argc, const char * argv[])
 {
+    FILE *fp;
     stackp=0;
-    c=0;
     c_p=0;
     pointer=1;
+    uint32_t count=0;
+    uint8_t input[SIZE_PROG];
 
-    printf("Braincube\n");
 
-    for(int i=1; i<=6; i++)
+    printf("Braincube Interpreter v. 0.1\n");
+    printf("%s\n",argv[1]);
+    if (argc >= 1 || (fp = fopen(argv[1], "r")) == NULL)
     {
-        for(int j=1; j<=9; j++)
-        {
-            cube[i][j]=i;
-        }
+        displayCube();
+        printf("No input file");
+        return 1;
     }
 
+    while ((input[count++] = getc(fp)) != EOF)
 
-    if(true==compile("|F|R|U|B|L|D")) printf("Compilation success\n");
+        for(int i=1; i<=6; i++)
+        {
+            for(int j=1; j<=9; j++)
+            {
+                cube[i][j]=i;
+            }
+        }
+
+
+    if(true==compile(input)) printf("Compilation success\n");
     displayCube();
     if(true==execute(100)) printf("\n Execution success\n");
     displayCube();
